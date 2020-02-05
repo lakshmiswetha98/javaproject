65785a03325739ebeca1264cbd9f267f7f951ed1
lakshmiswetha98 <60031019+lakshmiswetha98@users.noreply.github.com>
2020-02-05 17:58:25 +0530

    Add files via upload

---
+++ spring-web-mvc/src/com/cts/training/spring/web/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.cts.training.spring.web;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+	@RequestMapping("/dashboard")
+	public String home() {
+		return "employee-home";
+    }
+}
